d0b9a128c2d9872d44630e0d27c3fc5614c49496
ThePolishCat <damianadam000@gmail.com>
2023-11-16 16:42:42 +0100

    modified ai command

--- SlashCommands/AI/askAI.js
+++ SlashCommands/AI/askAI.js
@@ -1,16 +1,11 @@
 const { EmbedBuilder } = require('discord.js');
-const OpenAI = require('openai');
-const ball = require("../../structures/Json/8ball.json")
-const emojis = require("../../settings/emojis")
-const em = require("../../settings/embed");
-const openai = new OpenAI({
-	apiKey: "pk-**********************************************",
-	basePath: "https://api.pawan.krd/v1/chat/completions",
-});
+const fetch = require('node-fetch');
+const config = require('../../settings/config.json')
+
 
 module.exports = {
-  name: "askAI",
-  description: 'ask AI | Ask any questions and the bot will answer for you in text.',
+  name: "ask",
+  description: 'ask AI',
   type: 1,
   options: [
     {
@@ -24,14 +19,32 @@ module.exports = {
   run: async(client, interaction, args) => {
     const { options } = interaction;
     const question = interaction.options.getString("question")
-    const response = await openai.chat.completions.createCompletion({
-      model: "text-davinci-003",
-      max_tokens: 100,
-      messages: [
-        {"role": "system", "content": "You are an helpful assistant."},
-        {"role": "user", "content": question}]
+    const response = await fetch('https://api.pawan.krd/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${config.tokenAI}`,
+          'Content-Type': 'application/json'
+        },
+        // body: '{\n    "model": "pai-001-light-beta",\n    "max_tokens": 100,\n    "messages": [\n        {\n            "role": "system",\n            "content": "You are an helpful assistant."\n        },\n        {\n            "role": "user",\n            "content": "Who are you?"\n        }\n    ]\n}',
+        body: JSON.stringify({
+          'model': 'pai-001-beta',
+          'max_tokens': 400,
+          'messages': [
+            {
+              'role': 'system',
+              'content': 'Jesteś przyjaznym asystentem, który używa maksymalnie 120 słów na raz. Mówisz po polsku. Piszesz jak najmniej tekstu.'
+            },
+            {
+              'role': 'user',
+              'content': question + '.Użyj 120 maksymalnie słów.'
+            }
+          ]
+        })
     });
+    const data = await response.json();
+
+    console.log(data.choices[0].message.content);
     
-    interaction.reply(response)
+    interaction.followUp(data.choices[0].message.content)
   }
 }
