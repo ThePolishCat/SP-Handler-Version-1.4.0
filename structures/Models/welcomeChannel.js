96d9a2d77e8571a973b7a8ff05c596d4eafada4e
ThePolishCat <damianadam000@gmail.com>
2023-11-16 21:00:38 +0100

    modified files

--- structures/Models/welcomeChannel.js
+++
@@ -1,9 +0,0 @@
-const mongo = require('mongoose');
-
-const Schema = mongo.Schema({
-  Guild: String,
-  Channel: String,
-  User: String,
-});
-
-module.exports = mongo.model('welcome-channel', Schema)
