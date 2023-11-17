0b2c372a5da67d826d155702a1164e6614cd5e6d
ThePolishCat <damianadam000@gmail.com>
2023-11-17 18:38:47 +0100

    new file:   SlashCommands/JBZD/spotifyChange.js 	modified:   events/Interaction/messageCreate.js 	new file:   settings/jbzd.json

---
+++ SlashCommands/JBZD/spotifyChange.js
@@ -0,0 +1,31 @@
+const { Client } = require("discord.js");
+const fs = require("fs");
+
+module.exports = {
+    name: 'spotifychannel',
+    description: 'setup spotify moving channel',
+    /** 
+     * @param {Client} client 
+     * @param {Interaction} interaction 
+     * @param {String[]} args 
+     */
+    run: async (client, interaction, args) => {
+        const jbzdConfigPath = 'settings/jbzd.json';
+        const jbzdConfig = require("../../settings/jbzd.json");
+
+        const guildID = interaction.guild.id;
+        const channelID = interaction.channel.id;
+
+        const guildConfigIndex = jbzdConfig.findIndex(config => config.guild === guildID);
+
+        if (guildConfigIndex !== -1) {
+            jbzdConfig.splice(guildConfigIndex, 1);
+            fs.writeFileSync(jbzdConfigPath, JSON.stringify(jbzdConfig, null, 2));
+            interaction.followUp("Removed channel from spotify move");
+        } else {
+            jbzdConfig.push({ guild: guildID, channel: channelID });
+            fs.writeFileSync(jbzdConfigPath, JSON.stringify(jbzdConfig, null, 2));
+            interaction.followUp("Added channel to spotify move");
+        }
+    }
+};
--- events/Interaction/messageCreate.js
+++ events/Interaction/messageCreate.js
@@ -1,34 +1,17 @@
 const client = require("../../index");
-const { EmbedBuilder, PermissionsBitField } = require("discord.js")
-const { prefix } = require("../../settings/config.json")
-const ec = require("../../settings/embed")
+const { EmbedBuilder, PermissionsBitField } = require("discord.js");
+const { prefix } = require("../../settings/config.json");
+const ec = require("../../settings/embed");
 
 client.on("messageCreate", async (message) => {
-    if (
-        message.author.bot ||
-        !message.guild ||
-        !message.content.toLowerCase().startsWith(prefix)
-    )
+    if (message.author.bot || !message.guild) {
         return;
-
-    const [cmd, ...args] = message.content
-        .slice(prefix.length)
-        .trim()
-        .split(/ +/g);
-
-    const command = client.commands.get(cmd.toLowerCase()) || client.commands.find(c => c.aliases?.includes(cmd.toLowerCase()));
-
-    if (!command) return;
-
-    if(command.userPerms) {
-        if(!message.member.permissions.has(PermissionsBitField.resolve(command.userPerms || []))) {
-            const userPerms = new EmbedBuilder()
-            .setTitle('**Permission Needed**')
-            .setColor(ec.wrong)
-            .setDescription(`🚫 ${message.author}, You don't have \`${command.userPerms}\` permissions to use this command!`)
-            return message.reply({ embeds: [userPerms] })
-        }
     }
 
-    await command.run(client, message, args);
+    const jbzdConfig = require("../../settings/jbzd.json");
+    if (message.content.includes("https://open.spotify.com") && jbzdConfig.some(config => config.guild === message.guild.id)) {
+        const channel = jbzdConfig.find((config) => config.guild === message.guild.id)?.channel;
+        const spotifyUrl = message.content.match(/https:\/\/open\.spotify\.com[^\s]*/g);
+        client.channels.cache.get(channel).send(spotifyUrl[0]);
+    }
 });
