dc2bf9a28c7ac7bfc5ecb61703ce0c0ffd78a410
ThePolishCat <damianadam000@gmail.com>
2023-11-24 20:42:53 +0100

    zmieniono:       SlashCommands/Utils/pinnedImages.js zmieniono:       package.json

--- SlashCommands/Utils/pinnedImages.js
+++ SlashCommands/Utils/pinnedImages.js
@@ -1,11 +1,12 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 const fs = require("fs");
-const archiver = require("archiver");
 const axios = require("axios");
+const FormData = require("form-data");
+const tar = require("tar");
 
 module.exports = {
     name: 'downloadpinned',
-    description: 'Download pinned images, create a zip, and send it to the channel.',
+    description: 'Download pinned images, create a tar archive, and send it to the channel.',
     /**
      * @param {Client} client
      * @param {Interaction} interaction
@@ -28,8 +29,7 @@ module.exports = {
         pinnedMessages.forEach(message => {
             message.attachments.forEach(attachment => {
                 if (attachment.contentType.startsWith('image')) {
-                    console.log(attachment.url);
-                    const filePath = `${downloadDir}/${message.id+"_"+attachment.name}`;
+                    const filePath = `${downloadDir}/${message.id + "_" + attachment.name}`;
                     downloadPromises.push(axios({
                         method: 'get',
                         url: attachment.url,
@@ -43,23 +43,36 @@ module.exports = {
 
         await Promise.all(downloadPromises);
 
-        // Create a zip file with higher compression
-        const zipFilePath = `./downloads/images.zip`;
-        const output = fs.createWriteStream(zipFilePath);
-        const archive = archiver('zip', {
-            zlib: { level: 9 } // Set compression level to 9 (maximum)
+        // Create a tar archive without compression
+        const tarFilePath = `./downloads/images.tar`;
+        await tar.create(
+            {
+                file: tarFilePath,
+                cwd: downloadDir,
+                sync: true,
+            },
+            fs.readdirSync(downloadDir)
+        );
+
+        // Create FormData and append the tar file
+        const formData = new FormData();
+        formData.append('file', fs.createReadStream(tarFilePath));
+
+        // Upload the tar file to file.io using FormData
+        const fileIoResponse = await axios.post('https://file.io?expires=1h', formData, {
+            headers: {
+                ...formData.getHeaders(),
+            },
         });
 
-        output.on('close', async () => {
-            // Send the zip file to the channel
-            await interaction.followUp({ files: [zipFilePath] });
+        // Get the file.io link
+        const fileIoLink = fileIoResponse.data.link;
 
-            // Remove downloaded images and the zip file
-            fs.rmSync(downloadDir, { recursive: true });
-        });
+        // Send the file.io link to the Discord channel
+        await interaction.followUp(`Here is the link to download the tar archive: ${fileIoLink}`);
 
-        archive.pipe(output);
-        archive.directory(downloadDir, false);
-        archive.finalize();
+        // Remove downloaded images and the tar file
+        //fs.rmSync(downloadDir, { recursive: true });
+        //fs.unlinkSync(tarFilePath);
     },
 };
