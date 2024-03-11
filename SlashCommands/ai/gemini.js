101fc56f96b37ff493611b71dcfd6521f17ce98a
ThePolishCat <damianadam000@gmail.com>
2024-03-11 14:02:54 +0100

    update

--- SlashCommands/ai/gemini.js
+++ SlashCommands/ai/gemini.js
@@ -28,7 +28,6 @@ module.exports = {
       const text = response.text();
       if (text.length > 2000) {
         const buffer = Buffer.from(text, 'utf-8');
-        //const attachment = new MessageAttachment(buffer, 'file.txt')
         await interaction.followUp({ files: [
           { attachment: buffer, name: 'file.txt' }
         ]});
